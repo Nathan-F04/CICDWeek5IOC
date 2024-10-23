63dbacaae964db837c11cd636f0ba20b9a458763
G00413711@atu.ie <G00413711@atu.ie>
2024-10-23 14:35:16 +0100

    Created Prdouct class, Product controller and Product service

---
+++ src/main/java/ie/atu/cicdweek6/ProductService.java
@@ -0,0 +1,17 @@
+package ie.atu.cicdweek6;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> myList = new ArrayList<>();
+    public List<Product> addProduct(Product product)
+    {
+        //imagine we have a connection to a database
+        myList.add(product);
+        return myList;
+    }
+}
